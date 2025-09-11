64b3cdee9be6eea8b7cd167128111d642947f977
Orsula <karmous.orsula@gmail.com>
2025-09-11 11:43:04 +0200

    Ajout de l'entité Client

---
+++ src/Entity/Client.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Prénom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $telephone = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $dateCréation = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $dateModification = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrénom(): ?string
+    {
+        return $this->Prénom;
+    }
+
+    public function setPrénom(?string $Prénom): static
+    {
+        $this->Prénom = $Prénom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?int $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDateCréation(): ?\DateTime
+    {
+        return $this->dateCréation;
+    }
+
+    public function setDateCréation(?\DateTime $dateCréation): static
+    {
+        $this->dateCréation = $dateCréation;
+
+        return $this;
+    }
+
+    public function getDateModification(): ?\DateTime
+    {
+        return $this->dateModification;
+    }
+
+    public function setDateModification(\DateTime $dateModification): static
+    {
+        $this->dateModification = $dateModification;
+
+        return $this;
+    }
+}
